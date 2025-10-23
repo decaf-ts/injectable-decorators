14d4b62f44fdeaea9e5554c12b3c50559e11344e
tvenceslau <venceslau.tiago@gmail.com>
2025-10-23 02:25:56 +0100

    patch - remove need for reflect-no-ci - after release preparation

--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,5 @@ export * from "./utils";
  * @memberOf module:injectable-decorators
  */
 export const VERSION = "##VERSION##";
-export const PACKAGE_NAME = "##PACKAGE_NAME##";
+export const PACKAGE_NAME = "##PACKAGE##";
 Metadata.registerLibrary(PACKAGE_NAME, VERSION);
